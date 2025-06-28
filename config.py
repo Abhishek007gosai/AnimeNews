0c99b2601fc54272f9fb4b4918c3ab36a818612c
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-06-29 04:59:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = os.getenv("23537462")
+API_HASH = os.getenv("c9599a5aa61ee8ca4f5e778d20c61f24'")
+BOT_TOKEN = os.getenv("")
 URL_A = os.getenv("URL_A")
 URL_B = os.getenv("URL_B")
 URL_C = os.getenv("URL_C")
-START_PIC = os.getenv("START_PIC")
-MONGO_URI = os.getenv("MONGO_URI")
-ADMINS = list(map(int, os.getenv("ADMINS", "").split()))
+START_PIC = os.getenv("https://litter.catbox.moe/m4c4wu.jpg")
+MONGO_URI = os.getenv("mongodb+srv://BladexRobot:az50i6VCbZnez34y@cluster0.8uydmmp.mongodb.net/")
+ADMINS = list(map(int, os.getenv("ADMINS", "7654385403").split()))
