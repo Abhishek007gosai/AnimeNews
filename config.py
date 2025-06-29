10c9102367976550aba5cb123204e4079c49a1da
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-06-29 21:21:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-API_HASH = os.getenv("API_HASH")
-API_HASH = os.environ("API_HASH")
-BOT_TOKEN = os.environ("BOT_TOKEN")
-#URL_A = os.getenv("URL_A")
-#URL_B = os.getenv("URL_B")
-#URL_C = os.getenv("URL_C")
+API_HASH = os.getenv("29245477")
+API_HASH = os.environ("0abc83883262245c90ca337b7a0375c4")
+BOT_TOKEN = os.environ("7831174760:AAFKWWGJA2McsmSlrZUnYvXfy_BIvMsPf70")
+URL_A = os.getenv("")
+URL_B = os.getenv("Anime_News_Arena")
+URL_C = os.getenv("URL_C")
 START_PIC = os.getenv("https://litter.catbox.moe/m4c4wu.jpg")
 MONGO_URI = os.getenv("")
 ADMINS = list(map(int, os.getenv("ADMINS", "7654385403").split()))
